78e76d77ab4dd960c3bad8a449fcafb2394dfecd
EastKazu <fhornest55@gmail.com>
2024-09-20 11:40:09 +0900

    検索用データの作成に伴いactivehashを修正

--- app/models/country.rb
+++ app/models/country.rb
@@ -4,6 +4,11 @@ class Country < ActiveHash::Base
     { id: 2, name: 'オーストリア' },
     { id: 3, name: 'ロシア' },
     { id: 4, name: 'チェコ' },
+    { id: 5, name: 'イタリア' },
+    { id: 6, name: 'フィンランド' },
+    { id: 7, name: 'デンマーク' },
+    { id: 8, name: 'フランス' },
+    { id: 9, name: 'イギリス' },
   ]
 
   include ActiveHash::Associations
